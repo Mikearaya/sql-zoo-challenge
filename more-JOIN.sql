572615d02acfc56badce408bb505f2604e309114
mikearaya <mikaelaraya12@gmail.com>
2020-05-01 16:32:23 +0300

    Add using-NULL queries

--- more-JOIN.sql
+++ more-JOIN.sql
@@ -149,3 +149,29 @@ WHERE
  actorid IN (
   SELECT id FROM actor
   WHERE name='Art Garfunkel'))
+
+/*
+Use CASE to show the name of each teacher followed 
+by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+*/
+
+SELECT teacher.name,
+CASE 
+    WHEN dept.id IN (1, 2) THEN 'Sci'
+    ELSE 'Art'  
+END AS teacher
+FROM teacher LEFT JOIN dept
+        ON (teacher.dept=dept.id)
+
+/*
+Use CASE to show the name of each teacher followed by 'Sci'
+if the teacher is in dept 1 or 2, show 'Art' 
+if the teacher's dept is 3 and 'None' otherwise.
+*/
+SELECT teacher.name,
+CASE 
+    WHEN dept.id IN (1, 2) THEN 'Sci'
+    WHEN dept.id = 3  THEN 'Art'
+    ELSE 'None'  
+END AS teacher
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
